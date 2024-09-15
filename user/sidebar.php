9693df1ab860792f0a1e1e32b9927ffbfcfd9938
PasinduAnjana <pasinduanjana000@gmail.com>
2024-09-16 01:48:19 +0530

    change file names

--- user/sidebar.php
+++ user/sidebar.php
@@ -1,6 +1,6 @@
 <div class="sidebar">
     <a class="<?php if ($title === 'Home') echo 'active'; ?>" href="index.php">Home</a>
-    <a class="<?php if ($title === 'View Stock') echo 'active'; ?>" href="stock_manage.php">View Stock</a>
+    <a class="<?php if ($title === 'View Stock') echo 'active'; ?>" href="stock_view.php">View Stock</a>
     <a class="<?php if ($title === 'Orders') echo 'active'; ?>" href="#">My Orders</a>
     <a class="<?php if ($title === 'Help') echo 'active'; ?>" href="#">Help</a>
 </div>
